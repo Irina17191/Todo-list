70e15d32fd30a5fdffd05091f5575281ad72e8b2
Iryna Honcharenko <irynahoncharenko9@gmail.com>
2024-07-24 12:19:17 +0300

    Add views and templates for Tag model

--- core/views.py
+++ core/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from core.models import Tag, Task
+
+
+def index(request):
+    tasks = Task.objects.all()
+
+    context = {
+        "title": 'TODO List',
+        "tasks": tasks,
+    }
+    return render(request, "core/index.html", context=context)
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    template_name = "core/tag_list.html"
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("core:tag-list")
+    template_name = "core/tag_form.html"
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("core:tag-list")
+    template_name = "core/tag_form.html"
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("core:tag-list")
+    template_name = "core/tag_confirm_delete.html"
